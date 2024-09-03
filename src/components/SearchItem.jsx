fdb2fe2e211e909c5dbacedcaa5fe97f4d0398ac
Dhanush Karthick <dhanukarthick15@gmail.com>
2024-09-03 21:07:42 +0530

    first commit

--- src/components/SearchItem.jsx
+++ src/components/SearchItem.jsx
@@ -3,26 +3,26 @@ import { useParams } from 'react-router-dom'
 import { items } from './Data';
 import Product from './Product';
 
-const SearchItem = ({cart, setCart}) => {
+const SearchItem = ({ cart, setCart }) => {
   // console.log(useParams())
-  const {term} = useParams();
+  const { term } = useParams();
   const [filterData, setFilterData] = useState([]);
 
   useEffect(() => {
-    const filteredData = () =>{
-      const data = items.filter((p)=>p.title.toLowerCase().includes(term.toLowerCase()));
+    const filteredData = () => {
+      const data = items.filter((p) => p.title.toLowerCase().includes(term.toLowerCase()));
       // console.log(data)
       setFilterData(data)
     }
 
     filteredData();
-    
+
   }, [term])
-  
+
 
 
   return (
-   <Product cart={cart} setCart={setCart} items={filterData} />
+    <Product cart={cart} setCart={setCart} items={filterData} />
   )
 }
 
